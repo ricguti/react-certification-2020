77bbb3eecabb0e53236f3d0d8ea18059b0dad09a
Ricardo Gutiérrez <25915250+ricguti@users.noreply.github.com>
2020-09-20 17:01:43 -0500

    Add NavBar component

---
+++ src/components/NavBar/NavBar.component.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+import SearchBar from '../Search';
+import { useAuth } from '../../providers/Auth';
+
+const TopNavBar = styled.div`
+  overflow: hidden;
+`;
+
+const PageLink = styled.div`
+  float: left;
+  display: block;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+`;
+
+const NavBar = (props) => {
+  const history = useHistory();
+  const { authenticated, logout } = useAuth();
+
+  function deAuthenticate(event) {
+    event.preventDefault();
+    logout();
+    history.push('/');
+  }
+
+  return (
+    <TopNavBar>
+      <PageLink>
+        <Link to="/">Home </Link>
+      </PageLink>
+
+      <PageLink>
+        <Link to="/">Favourites</Link>
+      </PageLink>
+
+      <SearchBar searchValue={props.searchValue} setSearchValue={props.setSearchValue} />
+
+      {authenticated ? (
+        <PageLink>
+          <Link to="/" onClick={deAuthenticate}>
+            ← logout
+          </Link>
+        </PageLink>
+      ) : (
+        <PageLink>
+          <Link to="/login">← login</Link>
+        </PageLink>
+      )}
+    </TopNavBar>
+  );
+};
+
+export default NavBar;
--- src/components/Search/Search.component.jsx
+++ src/components/Search/Search.component.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  float: right;
+  display: block;
+  padding: 0.5em;
+  margin: 0.5em;
+  color: black;
+  background: white;
+  border: none;
+  border-radius: 3px;
+`;
 
 const SearchBar = (props) => {
   return (
     <div>
-      <input
+      <Input
         id="searchValue"
         value={props.searchValue}
         placeholder="Search a video..."
