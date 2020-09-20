9854d196a22a800ecabd0ab1d58498a0a8cdaef2
Ricardo Gutiérrez <25915250+ricguti@users.noreply.github.com>
2020-09-20 15:56:35 -0500

    Use props id in VideoItem component

--- src/components/VideoItem/VideoItem.component.jsx
+++ src/components/VideoItem/VideoItem.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const VideoItem = (props) => {
   return (
-    <div id="id">
+    <div id={props.id}>
       <img src={props.image} alt="a video" />
       <p>{props.title}</p>
       <p>{props.description}</p>
