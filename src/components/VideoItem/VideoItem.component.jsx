42f0f33d8907469460cead81c43924e78fc42969
Ricardo Gutiérrez <25915250+ricguti@users.noreply.github.com>
2020-09-19 21:46:20 -0500

    Add Video List component

--- src/components/VideoItem/VideoItem.component.jsx
+++ src/components/VideoItem/VideoItem.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const VideoItem = (props) => {
   return (
-    <div>
+    <div id="id">
       <img src={props.image} alt="a video" />
       <p>{props.title}</p>
       <p>{props.description}</p>
