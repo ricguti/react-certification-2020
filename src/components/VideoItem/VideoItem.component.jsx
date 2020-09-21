1c8665db235e552c63fcfe496de5691c7183d3dd
Ricardo Gutiérrez <25915250+ricguti@users.noreply.github.com>
2020-09-20 19:54:43 -0500

    Style VideoItem component

--- src/components/VideoItem/VideoItem.component.jsx
+++ src/components/VideoItem/VideoItem.component.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  border-radius: 5px;
+  display: flex;
+  margin: 10px;
+  background-color: white;
+
+  &:hover {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+  }
+
+  & img {
+    border-radius: 5px 5px 0 0;
+  }
+`;
+
+const Container = styled.div`
+  padding: 2px 16px;
+`;
 
 const VideoItem = (props) => {
   return (
-    <div id={props.id}>
+    <Card id={props.id}>
       <img src={props.image} alt="a video" />
-      <p>{props.title}</p>
-      <p>{props.description}</p>
-    </div>
+      <Container>
+        <h4>
+          <b>{props.title}</b>
+        </h4>
+        <p>{props.description}</p>
+      </Container>
+    </Card>
   );
 };
 
