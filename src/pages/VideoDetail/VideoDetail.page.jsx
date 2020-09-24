d0d3ab8b215edb000aede954c26858493e37c1f1
Ricardo Gutiérrez <25915250+ricguti@users.noreply.github.com>
2020-09-23 23:19:40 -0500

    Display selected video in VideoDetail page

--- src/pages/VideoDetail/VideoDetail.page.jsx
+++ src/pages/VideoDetail/VideoDetail.page.jsx
@@ -1,11 +1,24 @@
 import React, { useContext } from 'react';
 import SelectedVideoContext from '../../providers/SelectedVideo/SelectedVideoContext';
+import Layout from '../../components/Layout';
 
 const VideoDetail = () => {
   // eslint-disable-next-line
   const [selectedId, _] = useContext(SelectedVideoContext);
 
-  return <h1>Video Detail Page {selectedId}</h1>;
+  return (
+    <Layout>
+      <iframe
+        width="800"
+        height="450"
+        allowFullScreen
+        frameBorder="0"
+        title={selectedId}
+        src={`https://www.youtube.com/embed/${selectedId}?controls=0&autoplay=1`}
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      />
+    </Layout>
+  );
 };
 
 export default VideoDetail;
